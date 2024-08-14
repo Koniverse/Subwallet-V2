5ae9e2bc8e3f192be2c2458d8abed188985e2362
Frenkie Nguyen <nguyentiendung.me@gmail.com>
2024-08-14 18:21:15 +0700

    [Issue 2959] rollback show swap confirmation

--- packages/extension-base/src/services/swap-service/handler/hydradx-handler.ts
+++ packages/extension-base/src/services/swap-service/handler/hydradx-handler.ts
@@ -185,7 +185,7 @@ export class HydradxHandler implements SwapBaseInterface {
 
       const userRequestChangeFeeToken = !!customFeeTokenSlug && !_isNativeTokenBySlug(customFeeTokenSlug);
 
-      if (userRequestChangeFeeToken || !customFeeTokenSlug) {
+      if (!userRequestChangeFeeToken) {
         const isCurrentFeeTokenNative = !currentFeeAssetId || nativeTokenAssetId === currentFeeAssetId;
 
         if (!isCurrentFeeTokenNative) {
