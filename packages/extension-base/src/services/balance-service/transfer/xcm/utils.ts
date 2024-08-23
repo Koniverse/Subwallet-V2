88ac4ff23a4f5fb4fe125403a2335dd229877e6a
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-08-23 15:15:41 +0700

    [Issue-3453] fix: update method XCM for mythos-statemint

--- packages/extension-base/src/services/balance-service/transfer/xcm/utils.ts
+++ packages/extension-base/src/services/balance-service/transfer/xcm/utils.ts
@@ -14,9 +14,6 @@ export function isUseTeleportProtocol (originChainInfo: _ChainInfo, destChainInf
     (['polkadot'].includes(destChainInfo.slug) && ['statemint'].includes(originChainInfo.slug)) ||
     (['kusama'].includes(destChainInfo.slug) && ['statemine'].includes(originChainInfo.slug)) ||
     (['rococo'].includes(destChainInfo.slug) && ['rococo_assethub'].includes(originChainInfo.slug));
-  const isXcmMythos =
-    (originChainInfo.slug === 'mythos' && destChainInfo.slug === 'statemint' && tokenSlug === 'mythos-NATIVE-MYTH') ||
-    (originChainInfo.slug === 'statemint' && destChainInfo.slug === 'mythos' && tokenSlug === 'statemint-LOCAL-MYTH');
 
-  return relayChainToSystemChain || systemChainToRelayChain || isXcmMythos;
+  return relayChainToSystemChain || systemChainToRelayChain;
 }
