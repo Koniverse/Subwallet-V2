bbe8641a48804c1e64a54f2c030024e020bccfd8
tunghp2002 <atsimet@gmail.com>
2024-09-25 10:41:45 +0700

    [Fixed] Extension - Update Authorization Token

--- packages/extension-base/src/services/earning-service/handlers/native-staking/config.ts
+++ packages/extension-base/src/services/earning-service/handlers/native-staking/config.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /* Bittensor Authorization Token */
-export const AuthorizationToken = '7hTzjMbs75WlKoLlpTsdeTnjY5DICafWv4icx';
+export const AuthorizationToken = '7hTzjMbs75WlKoLlpTsdeTnjY5DICafWv4icxq9qah4DBmvltqIQpzvrbh57kurm';
 
 /* Get API key from https://app.taostats.io/ - Currently in beta */
 /* Use in https://docs.taostats.io/reference/welcome-to-the-taostats-api */
